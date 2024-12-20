02f4523bea00c1b3b2030747b4d5b0f8ce431531
jayjayKimm <kjzon90@gmail.com>
2024-12-20 16:22:35 +0900

    build bug fix

--- tennis365-management/src/app/api/player/route.js
+++ tennis365-management/src/app/api/player/route.js
@@ -1,13 +1,13 @@
-import { NextResponse } from 'next/server';
-import pool from '../../../../db';
+// import { NextResponse } from 'next/server';
+// import pool from '../../../../db';
 
-export async function GET(req,res) {
-    try{
-        const result = await pool.query('SELECT * FROM player');
+// export async function GET(req,res) {
+//     try{
+//         const result = await pool.query('SELECT * FROM player');
    
-        return NextResponse.json(result.rows);
-    } catch(error) {
-        console.error('Error running query', error);
-        res.status(500).json({error:'Internal Server Error'});
-    }
-}
+//         return NextResponse.json(result.rows);
+//     } catch(error) {
+//         console.error('Error running query', error);
+//         res.status(500).json({error:'Internal Server Error'});
+//     }
+// }
